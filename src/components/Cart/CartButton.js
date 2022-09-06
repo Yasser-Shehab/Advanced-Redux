8d897be45dd24a441147101cb1825d45aadadb05
AquaApple <yassershehab9@gmail.com>
2022-09-06 22:09:51 +0200

    Created Toggle Cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
-import classes from './CartButton.module.css';
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/uiSlice";
+import { useDispatch, useSelector } from "react-redux/es/exports";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./uiSlice";
-import cartReducer from "./cart";
+import cartSlice from "./cartSlice";
 
 const store = configureStore({
-  reducer: { cart: cartReducer, ui: uiSlice },
+  reducer: { cart: cartSlice, ui: uiSlice },
 });
 
 export default store;
